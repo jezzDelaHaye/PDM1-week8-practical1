a3eb1e743dbaac3551e588dfc0088482a1c1a94c
Jeremy Southern <jezzd11@outlook.com>
2025-11-21 07:54:43 +0000

    completed all exercises

--- exercise1-4/sketch.js
+++ exercise1-4/sketch.js
@@ -1,15 +1,41 @@
+let mic, ftt;
+
+function setup() 
+{
+    createCanvas(600, 600);
+    fft = new p5.FFT(0.8,512);
+    mic = new p5.AudioIn();
+    getAudioContext().suspend();
+    mic.start();
+    fft.setInput(mic);
+}
+
+function draw() 
+{
+    createCanvas(600,600);
+    drawCharacter();
+    console.log(mic.getLevel())
+}
+
 function drawCharacter()
 {
-    //headbody
-    ellipse(mouseX,mouseY,100,200)
-    circle(mouseX,mouseY - 140,80)
+    //body
+    ellipse(width/2, height/2, 100, 200)
+
+    //head
+    circle(width/2, (height/2 - 140) - (mic.getLevel()*2), 80)
 
     //legs
-    ellipse(mouseX + 30, mouseY + 180,50,190)
-    ellipse(mouseX - 30 ,mouseY + 180,50,190)
+    ellipse(width/2 + 30, height/2 + 180, 50, 190)
+    ellipse(width/2 - 30, height/2 + 180, 50, 190)
 
     //arms
-    ellipse(mouseX+60,mouseY + 15,50,190)
-    ellipse(mouseX-40,mouseY + 15,50,190)
+    ellipse(width/2 + 60, height/2 + 15, 50, 190)
+    ellipse(width/2 - 60, height/2 + 15, 50, 190)
 
+}
+
+function mouseClicked()
+{
+    userStartAudio();
 }
